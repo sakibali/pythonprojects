8d17eb620f978181559b31e3fad1e41b8aae587e
Sakib Ali <bhatsakibali@gmail.com>
2025-07-22 11:56:27 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+#
+# adding = num1 + num2
+# subtract = num1 - num2
+# multi = num1 * num2
+# divi = num1/num2
+#
+# print("Addition:", adding)
+# print("Subtraction:", subtract)
+# print("Multiplication:", multi)
+# print("Division:", divi)
+
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+
+fullname = name1 + " " + name2
+
+print("Hello, ", fullname,"! Welcome to Python program.")
